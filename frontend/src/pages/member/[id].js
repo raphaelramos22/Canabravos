0f19637eb441ecf100d3b568089fe2b3c4fbfbe0
Raphael <rafa.ramos_22@hotmail.com>
2023-07-17 22:04:46 -0300

    feat: adicionando mais informação para o perfil de cada membro

--- frontend/src/pages/member/[id].js
+++ frontend/src/pages/member/[id].js
@@ -12,14 +12,16 @@ export default function Profile({repo}) {
   return (
     <>
       {repo?
-        <div class=" h-screen flex justify-around bg-orange-600 pt-3">
-          <div class="h-80 border-8 grid content-evenly place-items-center border-blue-950">
+        <div class=" h-screen bg-orange-600 pt-3">
+          <div>
             <img 
               src={repo.image}
               alt={repo.displayName}
-              class= "w-52 h-52"
+              class= "w-80 h-80"
               />
             <h1 class="text-center text-xl">{repo.displayName}</h1>
+            <p class="text-center text-xl">Time: {repo.soccerTeam}</p>
+            <p class="text-center text-xl">Profisão: {repo.professions}</p>
             <p class="text-center text-xl">{repo.maritalStatus}</p>
             <Link href={repo.instagram}>
               <img class="text-center" src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white" target="_blank"/>
