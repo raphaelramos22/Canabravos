3ceb2602bb32ee349d6d0b58277991bd5ebfeb66
Raphael <rafa.ramos_22@hotmail.com>
2023-07-17 22:00:05 -0300

    feat:funcionalidade de filtro de busca atraves do nome do membro no front-end da pagina canabrvos

--- frontend/src/pages/canabravos.js
+++ frontend/src/pages/canabravos.js
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import React from 'react'
+
 
 export const getStaticProps = async () => {
   const res = await fetch('http://localhost:3008/member')
@@ -7,10 +9,32 @@ export const getStaticProps = async () => {
   return { props: { repo } }
 }
 
+
+
 export default function Page({ repo }) {
+  const [rest, setRest] = React.useState("");
+  const [resut, setResut] = React.useState("");
+  function filterName(repo) {
+    return repo.displayName === rest
+  }
   return  (
     <div class=" h-screen flex justify-around bg-orange-600 pt-3">
-      {repo.map(({id,displayName, instagram, maritalStatus, image},i)=>(
+     <label>
+        <input value={rest} onChange={(e) => setRest(e.target.value) } type="text" id="User" name="Name" />
+        <button onClick={()=>setResut(repo.find(filterName), setRest(""))} >Buscar</button>
+      </label>
+      {resut?
+      <Link key={resut.id} href={`member/${resut.id}`}>
+      <div class="h-80 border-8 grid content-evenly place-items-center border-blue-950">
+        <img 
+          src={resut.image}
+          alt={resut.displayName}
+          class= "w-52 h-52"
+          />
+        <h1 class="text-center text-xl">{resut.displayName}</h1>
+      </div>
+    </Link> 
+      :repo.map(({id,displayName, image},i)=>(
       <Link key={i} href={`member/${id}`}>
         <div class="h-80 border-8 grid content-evenly place-items-center border-blue-950">
           <img 
