bd421c2488f9295ab16786ffd9414cbfb377ccf4
Raphael <rafa.ramos_22@hotmail.com>
2023-07-25 14:15:11 -0300

    feat: adicionando adicionando msg de erro no memberfilterId caso o id não existe

--- backend/src/services/memberService.js
+++ backend/src/services/memberService.js
@@ -7,6 +7,11 @@ const memberAll = async () => {
 
 const memberFilterId = async (id) => {
   const result = await Member.findOne({where: { id }});
+  if (!result) {
+    const e = new Error('User does not exist');
+    e.code = 'NotFoundError';
+    throw e;
+  }
   return result;
 };
 module.exports = {
